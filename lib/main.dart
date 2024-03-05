e06dd1ef09c2ffcc5cda5a270031c9507505f134
Alec Yalcin <alecyalcin@gmail.com>
2024-03-05 14:18:42 -0300

    Atualização Cervejaria

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,57 @@ import 'package:flutter/material.dart';
 
 void main() {
   MaterialApp app = MaterialApp(
-    theme: ThemeData(primarySwatch: Colors.deepPurple),
+    theme: ThemeData(primarySwatch: Colors.deepOrange),
     home: Scaffold(
-      appBar: AppBar(title: const Text("AppBar Test")),
-      body: const Center(
-        child: Text("Hello, world!")
-      ),
-      bottomNavigationBar: const Text("Botão 1"),
+      appBar: AppBar(title: const Center(
+        child: Text("Cerveijaria do Turco")
+      )),
+      body: const Column(
+        children: [
+          ExpansionTile(
+            title: Text("Belvedere"),
+            subtitle: Text("Vodka"),
+            children: [
+              Center(
+                child: Column(
+                  children: [
+                    Text("Teor Alcólico: 40%"),
+                    Text("País de Origem: Polônia"),
+                  ],
+                ) 
+              ),
+            ],
+          ),
+          ExpansionTile(
+            title: Text("Sierra Nevada"),
+            subtitle: Text("Cerveja"),
+            children: [
+              Center(
+                child: Column(
+                  children: [
+                    Text("Teor Alcólico: 4%~8%"),
+                    Text("País de Origem: Estados Unidos"),
+                  ],
+                ) 
+              ),
+            ],
+          ),
+          ExpansionTile(
+            title: Text("Leblon"),
+            subtitle: Text("Cachaça"),
+            children: [
+              Center(
+                child: Column(
+                  children: [
+                    Text("Teor Alcólico: 30%"),
+                    Text("País de Origem: Brasil"),
+                  ],
+                ) 
+              ),
+            ],
+          ),
+        ],
+      )
     ));
   runApp(app);
 }
